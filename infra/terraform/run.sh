a9cfed183dda74f21c1ed6301f994ca2f7cdfa7a
Shashank Reddy Sunkara <inusasunny@gmail.com>
2022-03-02 10:25:25 +0530

    Update run.sh

--- infra/terraform/run.sh
+++ infra/terraform/run.sh
@@ -3,5 +3,5 @@ terraform_path="terraform"
 $terraform_path init
 $terraform_path destroy -auto-approve
 $terraform_path init
-$terraform_path plan
+$terraform_path plan -out k8.plan
 $terraform_path apply -auto-approve
